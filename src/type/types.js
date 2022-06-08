bb6a93e82552777e8b907918122c4f0b7f50f9db
Gustavo <gar.santeliz@gmail.com>
2022-06-07 22:15:41 -0400

    types wrote to manage reducers efficiently

--- src/type/types.js
+++ src/type/types.js
@@ -2,7 +2,7 @@ export const types = {
     uiOpenModal: '[ui] Open modal',
     uiCloseModal: '[ui] Close modal',
 
-    eventAddNew: '[event] Set active',
-    eventSetActive: '[event] Event added'
+    eventAddNew: '[event] New event',
+    eventSetActive: '[event] Set active'
 
 }
